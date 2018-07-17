e237b5751d6a22eeecb9a6a70c2cafcf33227504
Lang <silentbalanceyh@126.com>
2018-07-17 15:48:18 +0800

    异步Rpc处理

--- spring-co/src/main/java/io/spring/up/epic/RpcClient.java
+++ spring-co/src/main/java/io/spring/up/epic/RpcClient.java
@@ -1,13 +1,13 @@
 package io.spring.up.epic;
 
 import io.grpc.Channel;
+import io.reactivex.Single;
 import io.spring.up.epic.fn.Fn;
 import io.spring.up.ipc.model.IpcRequest;
 import io.spring.up.ipc.model.IpcResponse;
 import io.spring.up.ipc.service.UnityServiceGrpc;
 import io.spring.up.model.Envelop;
 import io.vertx.core.json.JsonObject;
-import org.springframework.scheduling.annotation.AsyncResult;
 
 import java.util.concurrent.Future;
 
@@ -43,15 +43,15 @@ public class RpcClient {
         return Rpc.outEnvelop(response);
     }
 
-    public Future<JsonObject> sendJson(final JsonObject data) {
+    public Single<JsonObject> sendJson(final JsonObject data) {
         final IpcRequest request = Rpc.in(data);
         final Future<IpcResponse> response = this.getFutureStub().unityCall(request);
-        return AsyncResult.forValue(Fn.getJvm(() -> Rpc.outJson(response.get()), response));
+        return Single.just(Fn.getJvm(() -> Rpc.outJson(response.get()), response));
     }
 
-    public Future<Envelop> sendJson(final Envelop data) {
+    public Single<Envelop> sendJson(final Envelop data) {
         final IpcRequest request = Rpc.in(data);
         final Future<IpcResponse> response = this.getFutureStub().unityCall(request);
-        return AsyncResult.forValue(Fn.getJvm(() -> Rpc.outEnvelop(response.get()), response));
+        return Single.just(Fn.getJvm(() -> Rpc.outEnvelop(response.get()), response));
     }
 }
