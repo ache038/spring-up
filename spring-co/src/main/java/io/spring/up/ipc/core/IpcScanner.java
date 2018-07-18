11cfaea284e8a87225d849cc781b482425ce62c1
Lang <silentbalanceyh@126.com>
2018-07-18 09:43:34 +0800

    Rpc服务开发完成

--- spring-co/src/main/java/io/spring/up/ipc/core/IpcScanner.java
+++ spring-co/src/main/java/io/spring/up/ipc/core/IpcScanner.java
@@ -6,13 +6,10 @@ import io.spring.up.annotations.Ipc;
 import io.spring.up.epic.Ut;
 import io.spring.up.epic.fn.Fn;
 import io.spring.up.log.Log;
-import io.spring.up.model.Envelop;
-import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -55,7 +52,7 @@ public class IpcScanner extends Thread {
         Log.info(LOGGER, "[ UP ] Ipc Scanner started: {0} for {1}", this.getName(), this.target);
         Fn.safeNull(() -> Observable.fromArray(this.target.getDeclaredMethods())
                         .filter(method -> method.isAnnotationPresent(Ipc.class))
-                        .filter(this::ensure)
+                        .filter(IpcSelector::filterMethod)
                         .map(method -> Single.just(method)
                                 .map(item -> item.getAnnotation(Ipc.class))
                                 .map(item -> Ut.invoke(item, "value"))
@@ -72,28 +69,5 @@ public class IpcScanner extends Thread {
     private String put(final String item) {
         PROXIES.put(item, this.reference);
         return item;
-    }
-
-    private boolean ensure(final Method method) {
-        final Class<?> type = method.getReturnType();
-        if (Envelop.class != type && JsonObject.class != type) {
-            Log.warn(LOGGER, "[ UP ] Ignored the method {0} because of Return Type: {1}",
-                    method.getName(), type.getName());
-            return false;
-        }
-        final Parameter[] parameters = method.getParameters();
-        if (1 != parameters.length) {
-            Log.warn(LOGGER, "[ UP ] Ignored the method {0} because of Parameter Length must be 1, current = {1}",
-                    method.getName(), parameters.length);
-            return false;
-        }
-        final Parameter parameter = parameters[0];
-        final Class<?> parameterType = parameter.getType();
-        if (Envelop.class != parameterType && JsonObject.class != parameterType) {
-            Log.warn(LOGGER, "[ UP ] Ignored the method {0} because of Parameter Type {1}",
-                    method.getName(), parameterType.getName());
-            return false;
-        }
-        return true;
     }
 }
