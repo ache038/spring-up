01b5f184bdf4eb97b8eb79ad10a114dcaeea392c
Lang <silentbalanceyh@126.com>
2018-08-08 14:38:52 +0800

    添加Mongo查询

--- spring-co/src/main/java/io/spring/up/query/fetcher/JpaFetcher.java
+++ spring-co/src/main/java/io/spring/up/query/fetcher/JpaFetcher.java
@@ -2,10 +2,7 @@ package io.spring.up.query.fetcher;
 
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.dsl.BooleanPath;
-import com.querydsl.core.types.dsl.DateTimePath;
 import com.querydsl.core.types.dsl.EntityPathBase;
-import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import io.spring.up.log.Log;
@@ -18,7 +15,6 @@ import io.zero.epic.Ut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class JpaFetcher<T> implements Fetcher<T> {
@@ -102,35 +98,10 @@ public class JpaFetcher<T> implements Fetcher<T> {
      */
     private JPAQuery<T> getOrderBy(final JPAQuery<T> query) {
         JPAQuery<T> result = query;
-        if (null != this.inquiry.getSorter()) {
-            final JsonObject sorter = this.inquiry.getSorter().toJson();
-            final List<OrderSpecifier<?>> specifiers = new ArrayList<>();
-            for (final String field : sorter.fieldNames()) {
-                final Boolean isAsc = sorter.getBoolean(field);
-                final Object path = Ut.field(this.entity, field);
-                final OrderSpecifier<?> specifier = this.getOrderSpecifier(path, isAsc);
-                if (null != specifier) {
-                    specifiers.add(specifier);
-                    Log.info(LOGGER, "[ UP ] [QE] Order By: field = {0}, asc = {1}", field, isAsc);
-                }
-            }
-            result = result.orderBy(specifiers.toArray(new OrderSpecifier[]{}));
+        final OrderSpecifier[] orderSpecifiers = FetcherUtil.getOrderSpecifier(this.inquiry, this.entity);
+        if (0 < orderSpecifiers.length) {
+            result = result.orderBy(orderSpecifiers);
         }
         return result;
-    }
-
-
-    @SuppressWarnings("unchecked")
-    private <I extends Comparable> OrderSpecifier<I> getOrderSpecifier(final Object path, final boolean asc) {
-        final Class<?> clazz = path.getClass();
-        // StringPath类型的排序
-        if (StringPath.class == clazz) {
-            return asc ? (OrderSpecifier<I>) ((StringPath) path).asc() : (OrderSpecifier<I>) ((StringPath) path).desc();
-        } else if (DateTimePath.class == clazz) {
-            return asc ? (OrderSpecifier<I>) ((DateTimePath) path).asc() : (OrderSpecifier<I>) ((DateTimePath) path).desc();
-        } else if (BooleanPath.class == clazz) {
-            return asc ? (OrderSpecifier<I>) ((BooleanPath) path).asc() : (OrderSpecifier<I>) ((BooleanPath) path).desc();
-        }
-        return null;
     }
 }
--- spring-co/src/main/java/io/spring/up/query/fetcher/MongoFetcher.java
+++ spring-co/src/main/java/io/spring/up/query/fetcher/MongoFetcher.java
@@ -1,17 +1,25 @@
 package io.spring.up.query.fetcher;
 
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.mongodb.morphia.MorphiaQuery;
+import io.spring.up.log.Log;
 import io.spring.up.query.Fetcher;
 import io.vertx.core.json.JsonObject;
 import io.vertx.up.atom.query.Inquiry;
+import io.vertx.up.atom.query.Pager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MongoFetcher<T> implements Fetcher<T> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoFetcher.class);
     private transient Inquiry inquiry;
     private transient EntityPathBase<T> entity;
 
     @Override
     public JsonObject search(final Predicate predicate) {
+        // TODO: Mongo查询
         return null;
     }
 
@@ -26,4 +34,31 @@ public class MongoFetcher<T> implements Fetcher<T> {
         this.inquiry = inquiry;
         return this;
     }
+
+    private MorphiaQuery<T> getPager(final MorphiaQuery<T> query) {
+        MorphiaQuery<T> result = query;
+        if (null != this.inquiry.getPager()) {
+            final Pager pager = this.inquiry.getPager();
+            Log.info(LOGGER, "[ UP ] [QE] Pagination: start = {0}, size = {1}", pager.getStart(), pager.getSize());
+            result = result.limit(pager.getSize()).offset(pager.getStart());
+        }
+        return result;
+    }
+
+
+    /**
+     * 排序
+     *
+     * @param query
+     * @return
+     */
+    private MorphiaQuery<T> getOrderBy(final MorphiaQuery<T> query) {
+        MorphiaQuery<T> result = query;
+        final OrderSpecifier[] orderSpecifiers = FetcherUtil.getOrderSpecifier(this.inquiry, this.entity);
+        if (0 < orderSpecifiers.length) {
+            result = result.orderBy(orderSpecifiers);
+        }
+        return result;
+    }
+
 }
