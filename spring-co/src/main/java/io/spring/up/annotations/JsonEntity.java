f2a411cf484116a0a5e9e82b5098f6dd4537add9
Lang <silentbalanceyh@126.com>
2018-08-13 15:02:18 +0800

    Remove Folder

--- spring-co/src/main/java/io/spring/up/annotations/JsonEntity.java
+++ spring-co/src/main/java/io/spring/up/annotations/JsonEntity.java
@@ -12,7 +12,7 @@ public @interface JsonEntity {
     String value();
 
     // 使用的验证目录地址，目录地址不提供时不可支持嵌套验证模式
-    String folder() default "rules";
+    String folder() default "";
 
     // 该参数是否为必须，如果必须不填写时则报错
     boolean required() default true;
